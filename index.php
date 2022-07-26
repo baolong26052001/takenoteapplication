001ae4903a166646882d39fe0a41796e402b1157
Dang Nhat Nguyen <danielnhatng@gmail.com>
2022-07-26 10:05:39 +0700

    PHP

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+/** @var Connection $connection */
+$connection = require_once 'pdo.php';
+// Read notes from database
+$notes = $connection->getNotes();
+
+$currentNote = [
+    'id' => '',
+    'title' => '',
+    'description' => ''
+];
+if (isset($_GET['id'])) {
+    $currentNote = $connection->getNoteById($_GET['id']);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Note Website</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" 
+    crossorigin="anonymous">
+    <link rel="stylesheet" href="app.css">
+</head>
+
+<body>
+<nav class="navbar navbar-dark bg-dark navbar-expand-sm">
+
+        <div class="row" style="width:100%; height:100%">
+            <div class="col-sm-6-md-3" style="width:0%; height:100%">
+                <a href=""><img src="logo.jpg" alt=""> </a>
+            </div>
+            <div class="col-sm-6-md-9" style="width:100%; height:100%; text-align:center" >
+                <H1 class="webname"> Note taking</H1>
+            </div>
+    </div>        
+</nav>
+
+
+<div class="main-body">
+    <form class="new-note" action="create.php" method="post">
+        <input type="hidden" name="id" value="<?php echo $currentNote['id'] ?>">
+        <input type="text" name="title" placeholder="Note title" autocomplete="off"
+               value="<?php echo $currentNote['title'] ?>">
+        <textarea name="description" cols="30" rows="4"
+                  placeholder="Note Description"><?php echo $currentNote['description'] ?></textarea>
+        <button>
+            <?php if ($currentNote['id']): ?>
+                Update
+            <?php else: ?>
+                New note
+            <?php endif ?>
+        </button>
+    </form>
+    <div class="notes">
+        <?php foreach ($notes as $note): ?>
+            <div class="note">
+                <div class="title">
+                    <a href="?id=<?php echo $note['id'] ?>">
+                        <?php echo $note['title'] ?>
+                    </a>
+                </div>
+                <div class="description">
+                    <?php echo $note['description'] ?>
+                </div>
+                <small><?php echo date('d/m/Y H:i', strtotime($note['create_date'])) ?></small>
+                <form action="delete.php" method="post">
+                    <input type="hidden" name="id" value="<?php echo $note['id'] ?>">
+                    <button class="close">X</button>
+                </form>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+</body>
+</html>
---
+++ notes.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jul 25, 2022 at 06:53 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `notes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+DROP TABLE IF EXISTS `notes`;
+CREATE TABLE IF NOT EXISTS `notes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `notes`
+--
+
+INSERT INTO `notes` (`id`, `title`, `description`, `create_date`) VALUES
+(1, 'group j', 'dota', '2022-07-25 06:52:47');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
